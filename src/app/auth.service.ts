0b419f695d6355fa730478d1fe633b72964cf91d
Mosh Hamedani <moshfegh@live.com.au>
2017-08-15 18:05:23 -0700

    Fix a bug (with logging out).

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/of'; 
 import * as firebase from 'firebase'; 
 
 @Injectable()
@@ -31,6 +32,10 @@ export class AuthService {
 
   get appUser$() : Observable<AppUser> {
     return this.user$
-      .switchMap(user => this.userService.get(user.uid))    
+      .switchMap(user => {
+        if (user) return this.userService.get(user.uid);
+
+        return Observable.of(null);
+      });    
   }
 }
