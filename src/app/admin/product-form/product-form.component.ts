6840363c87d16a0da8ff8d80052dbca1ec5c91fe
Mosh Hamedani <moshfegh@live.com.au>
2017-08-18 16:10:41 -0700

    Implement deleting a product.

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -33,6 +33,13 @@ export class ProductFormComponent implements OnInit {
     this.router.navigate(['/admin/products']);
   }
 
+  delete() {
+    if (!confirm('Are you sure you want to delete this product?')) return;
+    
+    this.productService.delete(this.id);
+    this.router.navigate(['/admin/products']);
+  }
+
   ngOnInit() {
   }
 
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -21,4 +21,8 @@ export class ProductService {
   update(productId, product) { 
     return this.db.object('/products/' + productId).update(product);
   }
+
+  delete(productId) { 
+    return this.db.object('/products/' + productId).remove();
+  }
 }
