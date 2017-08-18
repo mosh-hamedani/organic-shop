48689e825a23452b4338f9f6b2743df8a2500c43
Mosh Hamedani <moshfegh@live.com.au>
2017-08-18 15:07:31 -0700

    Display the list of products.

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,3 +1,4 @@
+import { ProductService } from './../../product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnInit {
+  products$;
 
-  constructor() { }
+  constructor(private productService: ProductService) { 
+    this.products$ = this.productService.getAll();
+  }
 
   ngOnInit() {
   }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,4 +9,8 @@ export class ProductService {
   create(product) { 
     return this.db.list('/products').push(product);
   }
+
+  getAll() {
+    return this.db.list('/products');
+  }
 }
