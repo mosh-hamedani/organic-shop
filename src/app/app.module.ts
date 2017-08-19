284c7f0915ae2ffca5dbca2dcd1812ab993156a3
Mosh Hamedani <moshfegh@live.com.au>
2017-08-19 15:28:08 -0700

    Display all the products on the home page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -54,7 +54,7 @@ import { ProductFormComponent } from './admin/product-form/product-form.componen
     AngularFireAuthModule,
     NgbModule.forRoot(),
     RouterModule.forRoot([
-      { path: '', component: HomeComponent },
+      { path: '', component: ProductsComponent },
       { path: 'products', component: ProductsComponent },
       { path: 'shopping-cart', component: ShoppingCartComponent },
       { path: 'login', component: LoginComponent },
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,3 +1,4 @@
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +6,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent  {
+  products$;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(productService: ProductService) {
+    this.products$ = productService.getAll();
   }
 
 }
