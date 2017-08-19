d3c9072c8de7dd14da9c05ed765905640db44f33
Mosh Hamedani <moshfegh@live.com.au>
2017-08-19 14:17:36 -0700

    Fix a bug with redirecting users.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   constructor(private userService: UserService, private auth: AuthService, router: Router) {
     auth.user$.subscribe(user => {
-      if (user) {
-        userService.save(user);
+      if (!user) return; 
 
-        let returnUrl = localStorage.getItem('returnUrl');
-        router.navigateByUrl(returnUrl);
-      }
+      userService.save(user);
+
+      let returnUrl = localStorage.getItem('returnUrl');
+      if (!returnUrl) return; 
+
+      localStorage.removeItem('returnUrl');
+      router.navigateByUrl(returnUrl);
     });
   }
 }
