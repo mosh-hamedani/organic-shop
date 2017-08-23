64e72de887043cbc344790954ed18ce77b0e254e
Mosh Hamedani <moshfegh@live.com.au>
2017-08-23 15:42:20 -0700

    Display the total price.

--- src/app/models/shopping-cart-item.ts
+++ src/app/models/shopping-cart-item.ts
@@ -1,6 +1,7 @@
 import { Product } from './product';
 
-export interface ShoppingCartItem {
-  product: Product;
-  quantity: number;
+export class ShoppingCartItem {
+  constructor(public product: Product, public quantity: number) {}
+
+  get totalPrice() { return this.product.price * this.quantity; }
 }
--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -4,8 +4,17 @@ export class ShoppingCart {
   items: ShoppingCartItem[] = [];
 
   constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) {
-    for (let productId in itemsMap)
-      this.items.push(itemsMap[productId]);
+    for (let productId in itemsMap) {
+      let item = itemsMap[productId];
+      this.items.push(new ShoppingCartItem(item.product, item.quantity));
+    }
+  }
+
+  get totalPrice() {
+    let sum = 0;
+    for (let productId in this.items) 
+      sum += this.items[productId].totalPrice;
+    return sum;
   }
   
   get totalItemsCount() {
