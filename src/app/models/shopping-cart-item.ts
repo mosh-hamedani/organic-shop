ff34aa02906fd0197b878e5ca173f6f7eba6b319
Mosh Hamedani <moshfegh@live.com.au>
2017-08-24 13:08:01 -0700

    Better object initialization.

--- src/app/models/shopping-cart-item.ts
+++ src/app/models/shopping-cart-item.ts
@@ -7,5 +7,9 @@ export class ShoppingCartItem {
   price: number; 
   quantity: number; 
 
+  constructor(init?: Partial<ShoppingCartItem>) {
+    Object.assign(this, init);
+  }
+
   get totalPrice() { return this.price * this.quantity; }
 }
--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -9,10 +9,7 @@ export class ShoppingCart {
     
     for (let productId in itemsMap) {
       let item = itemsMap[productId];
-      let x = new ShoppingCartItem();
-      Object.assign(x, item);
-      x.$key = productId;
-      this.items.push(x);
+      this.items.push(new ShoppingCartItem({ ...item, $key: productId }));
     }
   }
 
