a8f94cc7d644c90bf0f4b63aff9cea58ab01f26a
Mosh Hamedani <moshfegh@live.com.au>
2017-08-25 13:21:09 -0700

    Clearing the shopping cart.

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -38,7 +38,7 @@ export class CheckOutComponent implements OnInit, OnDestroy {
 
   async placeOrder() {
     let order = new Order(this.userId, this.shipping, this.cart);
-    let result = await this.orderService.storeOrder(order);
+    let result = await this.orderService.placeOrder(order);
     this.router.navigate(['/order-success', result.key]);
   }    
 }
--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -1,12 +1,15 @@
+import { ShoppingCartService } from './shopping-cart.service';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class OrderService {
 
-  constructor(private db: AngularFireDatabase) { }
+  constructor(private db: AngularFireDatabase, private shoppingCartService: ShoppingCartService) { }
 
-  storeOrder(order) {
-    return this.db.list('/orders').push(order);
+  async placeOrder(order) {
+    let result = await this.db.list('/orders').push(order);
+    this.shoppingCartService.clearCart();
+    return result;
   }
 }
