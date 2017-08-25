98692d6314f032fd8633b5052c37546254e8fadc
Mosh Hamedani <moshfegh@live.com.au>
2017-08-25 12:52:17 -0700

    Refactoring: Extract an Order Model

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -4,6 +4,7 @@ import { Subscription } from 'rxjs/Subscription';
 import { ShoppingCart } from './../models/shopping-cart';
 import { ShoppingCartService } from './../shopping-cart.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Order } from "../models/order";
 
 @Component({
   selector: 'app-check-out',
@@ -34,23 +35,7 @@ export class CheckOutComponent implements OnInit, OnDestroy {
   }
 
   placeOrder() {
-    let order = {
-      userId: this.userId,
-      datePlaced: new Date().getTime(),
-      shipping: this.shipping,
-      items: this.cart.items.map(i => {
-        return {
-          product: {
-            title: i.title,
-            imageUrl: i.imageUrl,
-            price: i.price
-          },
-          quantity: i.quantity,
-          totalPrice: i.totalPrice
-        }
-      })
-    };
-
+    let order = new Order(this.userId, this.shipping, this.cart);
     this.orderService.storeOrder(order);
   }    
 }
---
+++ src/app/models/order.ts
@@ -0,0 +1,22 @@
+import { ShoppingCart } from './shopping-cart';
+
+export class Order { 
+  datePlaced: number; 
+  items: any[];
+
+  constructor(public userId: string, public shipping: any, shoppingCart: ShoppingCart) {
+    this.datePlaced = new Date().getTime();
+
+    this.items = shoppingCart.items.map(i => {
+      return {
+        product: {
+          title: i.title,
+          imageUrl: i.imageUrl,
+          price: i.price
+        },
+        quantity: i.quantity,
+        totalPrice: i.totalPrice
+      }
+    })    
+  }
+}
